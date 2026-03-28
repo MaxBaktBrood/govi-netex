06d1768b182bc4eba28644cbfebb5d5ecb7fe2ab
MaxBaktBrood <maxbaktbrood@vivaldi.net>
2026-03-29 00:27:29 +0100

    URL downloads possible

---
+++ url_source.py
@@ -0,0 +1,28 @@
+
+
+import xml.etree.ElementTree as ET
+import geopandas
+import pygml
+import json
+import os
+from zipfile import ZipFile
+import gzip
+import requests
+from io import BytesIO
+from netex import Netex
+
+def getNetexViaURL(url:str):
+    netex_request = requests.get(
+        url
+    )
+
+    if not netex_request.ok:
+        raise Exception(f'Netex request geweigerd: {netex_request.status_code}')
+    
+    netex_zip = ZipFile(BytesIO(netex_request.content))
+    for index, name in enumerate(netex_zip.namelist()):
+        print(f'ZIP: Bestand {index}/{len(netex_zip.namelist())}: {name}')
+        if os.path.splitext(name)[1] == '.xml':
+            file = netex_zip.open(name, 'r')
+            content = file.read()
+            netex = Netex(str_content=content)
