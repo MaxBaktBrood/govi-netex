8de98e5167b7d04b65ac519f3c2305cc0003cf91
MaxBaktBrood <maxbaktbrood@vivaldi.net>
2026-03-29 18:08:14 +0200

    user decides what is processed

--- index.py
+++ index.py
@@ -11,6 +11,7 @@ from si_source import getNetexSI
 from nl_source import getNetexNL
 from url_source import getNetexViaURL
 from plot import plot_layer
+import sys
 
 input_folder = './input'
 output_folder = './output'
@@ -25,47 +26,59 @@ if os.path.exists('./secrets.json'):
         open('secrets.json', 'r').read()
     )
 
-# print(f'Verwerken van Sloveense NeTEx')
-# for link, content in getNetexSI(secrets_file):
-#     print(f'Verwerken van {link}...')
-#     Netex(str_content=content)
+data_sources = [
+    "SI", "NL", "N", "FIN", "S", "INPUT"
+]
 
-# print(f'Verwerken van Nederlandse NeTEx')
-# getNetexNL(secrets=secrets_file)
+if len(sys.argv) > 1:
+    data_sources = sys.argv[1].split(",")
+    data_sources = map(lambda x: x.upper(), data_sources)
 
-# print(f'Verwerken van Noorse NeTEx')
-# getNetexViaURL("https://storage.googleapis.com/marduk-production/outbound/netex/rb_norway-aggregated-netex.zip")
+if 'SI' in data_sources:
+    print(f'Verwerken van Sloveense NeTEx')
+    for link, content in getNetexSI(secrets_file):
+        print(f'Verwerken van {link}...')
+        Netex(str_content=content)
 
-# print(f'Verwerken van Finse NeTEx')
-# getNetexViaURL("https://mobility.mobility-database.fintraffic.fi/static/finland_netex.zip")
+if 'NL' in data_sources:
+    print(f'Verwerken van Nederlandse NeTEx')
+    getNetexNL(secrets=secrets_file)
 
-if 'S_api_key_national' in secrets_file:
+if 'N' in data_sources:
+    print(f'Verwerken van Noorse NeTEx')
+    getNetexViaURL("https://storage.googleapis.com/marduk-production/outbound/netex/rb_norway-aggregated-netex.zip")
+
+if 'FIN' in data_sources:
+    print(f'Verwerken van Finse NeTEx')
+    getNetexViaURL("https://mobility.mobility-database.fintraffic.fi/static/finland_netex.zip")
+
+if 'S' in data_sources and 'S_api_key_national' in secrets_file:
     print(f'Verwerken van Zweedse NeTEx')
     getNetexViaURL(f"https://opendata.samtrafiken.se/netex-sweden/sweden.zip?key={secrets_file['S_api_key_national']}")  
 
+if 'INPUT' in data_sources:
+    print(f'Verwerken van de inputmap')
 
-print(f'Verwerken van de inputmap')
+    for item in os.listdir(input_folder):
+        path = os.path.join(input_folder, item)
+        
+        split_path = os.path.splitext(path)
+        print(f'Verwerken van {split_path[0]}')
 
-for item in os.listdir(input_folder):
-    path = os.path.join(input_folder, item)
-    
-    split_path = os.path.splitext(path)
-    print(f'Verwerken van {split_path[0]}')
-
-    if split_path[1] == '.zip':
-        netex_zip = ZipFile(path)
-        for index, name in enumerate(netex_zip.namelist()):
-            print(f'ZIP: Bestand {index}/{len(netex_zip.namelist())}: {name}')
-            if os.path.splitext(name)[1] == '.xml':
-                file = netex_zip.open(name, 'r')
-                content = file.read()
-                netex = Netex(str_content=content)
+        if split_path[1] == '.zip':
+            netex_zip = ZipFile(path)
+            for index, name in enumerate(netex_zip.namelist()):
+                print(f'ZIP: Bestand {index}/{len(netex_zip.namelist())}: {name}')
+                if os.path.splitext(name)[1] == '.xml':
+                    file = netex_zip.open(name, 'r')
+                    content = file.read()
+                    netex = Netex(str_content=content)
 
 
-    if split_path[1] == '.gz':
-        file = gzip.open(path, 'r')
-        content = file.read()
-        netex = Netex(str_content=content)
+        if split_path[1] == '.gz':
+            file = gzip.open(path, 'r')
+            content = file.read()
+            netex = Netex(str_content=content)
 
 print('Afbeelding maken...')
 plot_layer(geopandas.read_file('netex.gpkg', layer='routes'), output_folder=output_folder)
