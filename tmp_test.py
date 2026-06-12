4d1c3f84723b5811375d1ac3bceffd26d0d41eff
Max Bakker <m.bakker2@gelderland.nl>
2026-06-12 06:38:25 +0000

    bugfixes

--- tmp_test.py
+++ tmp_test.py
@@ -1,23 +1,23 @@
 from netex_json.netex_json import NetexJSON
 import pandas as pd
-import json
+import orjson as json
 
 
 
-netex_json = NetexJSON()
+netex_json = NetexJSON(language='nl')
 
 json_lines = netex_json.line_information()
 
 region_data = dict(map(
-    lambda x: (x[0], x[1]),
+    lambda x: (str(x[0]), x[1]),
     pd.read_excel(f'./gld_input/other/Regio_Lookup.xlsx').to_records(index=None)
 ))
 
 json_lines_per_network = netex_json.divide_lines_by_network(json_lines)
 json_lines_per_region = netex_json.divided_lines_per_region(json_lines_per_network, region_data)
 
-open(f'./output/lines.json', 'w').write(json.dumps(json_lines_per_region))
+open(f'./output/lines.json', 'wb').write(json.dumps(json_lines_per_region))
 
-# netex_json.to_json(lines=json_lines)
+netex_json.to_json(lines=json_lines)
 
 
