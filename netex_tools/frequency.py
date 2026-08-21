7b14c91ebab1c81962ba2fd4dda783d115f99719
Max Bakker <m.bakker2@gelderland.nl>
2026-08-21 09:56:35 +0000

    summaries for calculating frequency

--- netex_tools/frequency.py
+++ netex_tools/frequency.py
@@ -3,6 +3,7 @@ from timetable import getLine
 from datetime import datetime, time, timedelta
 import orjson as json
 import sys
+from statistics import median
 
 db_path = './output/netex.db'
 
@@ -24,7 +25,7 @@ SELECT id FROM lines
 
     con.close()
 
-    result = {}
+    result_day_to_day = {}
 
     for line_id in lines:
         line_id = line_id[0]
@@ -55,7 +56,7 @@ SELECT id FROM lines
                                 hours, minutes, seconds = [float(val ) for val in line_data['journeys'][departure['journey']]['starting_time'].split(':')]
                                 timestamp = date_tracker + timedelta(hours=hours, minutes=minutes, seconds=seconds) + timedelta(seconds=departure['departure'])
                                 day = departures_per_day.setdefault(date_tracker.isoformat(), [])
-                                if timestamp in day: raise Exception('Al in dag: ' + timestamp)
+                                if timestamp in day: print(line_id + ': al in dag: ' + timestamp.isoformat())
                                 day.append(timestamp)
 
                             date_tracker = date_tracker + timedelta(days=1)
@@ -71,19 +72,63 @@ SELECT id FROM lines
 
                     in_range = len(list(filter(range_filter, timestamps)))
 
-                    frequency = in_range ((range_to - range_from).total_seconds() / 3600)
+                    frequency = in_range / ((range_to - range_from).total_seconds() / 3600)
 
-                    result_line = result.setdefault(line_id, {})
+                    result_line = result_day_to_day.setdefault(line_id, {})
                     result_direction = result_line.setdefault(direction_key, {})
                     result_quay = result_direction.setdefault(quay_key, {})
                     result_quay[date] = frequency
 
-                    if frequency > 2:
-                        open('./output/frequencies.json', 'wb').write(json.dumps(departures_per_day))
-                        raise Exception('hoi')
+                    # if frequency > 2:
+                    #     open('./output/frequencies.json', 'wb').write(json.dumps(departures_per_day))
+                    #     raise Exception('hoi')
 
+    result_summary = {}
 
-        open('./output/frequencies.json', 'wb').write(json.dumps(result))
+    for line_key in result_day_to_day:
+        line = result_day_to_day[line_key]
+
+        line_summary = {}
+
+        for direction_key in line:
+            direction = line[direction_key]
+            direction_summary = {}
+
+            for quay_key in direction:
+                quay = direction[quay_key]
+                quay_summary = {}
+
+                for date_key in quay:
+                    date = datetime.fromisoformat(date_key)
+                    frequency = quay[date_key]
+
+                    weekday_key = 'weekdays'
+                    match date.weekday():
+                        case 5: weekday_key = 'saturday'
+                        case 6: weekday_key = 'sun- and holidays'
+
+                    summary = quay_summary.setdefault(weekday_key, [])
+                    summary.append(frequency)
+
+                for weekday_key in quay_summary:
+                    summary = direction_summary.setdefault(weekday_key, [])
+                    summary.extend(quay_summary[weekday_key])
+
+            for weekday_key in direction_summary:
+                summary = line_summary.setdefault(weekday_key, [])
+                summary.extend(direction_summary[weekday_key])
+
+        for weekday_key in line_summary:
+            line_summary[weekday_key] = {
+                'min':min(line_summary[weekday_key]),
+                'max':max(line_summary[weekday_key]),
+                'median':median(line_summary[weekday_key]),
+                'average':sum(line_summary[weekday_key]) / len(line_summary[weekday_key])
+            }
+
+        result_summary[line_key] = line_summary
+
+    open('./output/frequencies.json', 'wb').write(json.dumps(result_summary))
 
 if __name__ == "__main__":
     if len(sys.argv) < 3: 
