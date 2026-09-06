9de7b186a3cf478a90cca0aee9b599e23c97e94b
MaxBaktBrood <maxbaktbrood@vivaldi.net>
2026-09-06 19:13:20 +0200

    bugfixes

--- netex_db/netex_db.py
+++ netex_db/netex_db.py
@@ -4,22 +4,22 @@ import psycopg2
 import typing
 import sqlite3
 
-def get_pg_con(secrets_file=None):
-    if os.path.exists('./secrets.json') or secrets_file is not None:
-        if secrets_file is None:
-            secrets_file = json.loads(
-                open('secrets.json', 'r').read()
+def get_pg_con(secrets=None, secrets_file_path='./secrets.json'):
+    if os.path.exists(secrets_file_path) or secrets is not None:
+        if secrets is None:
+            secrets = json.loads(
+                open(secrets_file_path, 'r').read()
             )
-
-        if 'POSTGRES_credentials' in secrets_file:
-            if not ('POSTGRES_available' in secrets_file and secrets_file['POSTGRES_available'] is False):
+    
+        if 'POSTGRES_credentials' in secrets:
+            if not ('POSTGRES_available' in secrets and secrets['POSTGRES_available'] is False):
 
                 credential_string = ""
-                for key in secrets_file['POSTGRES_credentials']:
-                    credential_string += f"{key}={str(secrets_file['POSTGRES_credentials'][key])} "
+                for key in secrets['POSTGRES_credentials']:
+                    credential_string += f"{key}={str(secrets['POSTGRES_credentials'][key])} "
 
                 con = psycopg2.connect(credential_string)
-
+                
                 return (con, credential_string)
             else: return None
         else: return None
@@ -27,9 +27,9 @@ def get_pg_con(secrets_file=None):
 
 class SQLiteQuerying:
     def query_all(self, cur: sqlite3.Cursor, q, params=()):
-        return cur.execute(q, params).fetchall()
+        return cur.execute(q.replace('%s', '?'), params).fetchall()
     def query_one(self, cur: sqlite3.Cursor, q, params=()):
-        return cur.execute(q, params).fetchone()
+        return cur.execute(q.replace('%s', '?'), params).fetchone()
     
     def __init__(self):
         pass
@@ -41,6 +41,5 @@ class PostgresQuerying:
     def query_one(self, cur, q, params=()):
         cur.execute(q, params)
         return cur.fetchone()
-
 
 
